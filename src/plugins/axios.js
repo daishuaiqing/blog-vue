262504ec488c3c8f9506415c342b4205d9eb6164
daishuaiqing <daishuaiqing@163.com>
2018-10-12 13:09:50 +0800

    配置生产，开发，测试环境的路径

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,14 +2,15 @@
 
 import Vue from 'vue';
 import axios from "axios";
+import baseUrl from './setBaseUrl';
 
 // Full config:  https://github.com/axios/axios#request-config
-// axios.defaults.baseURL = process.env.baseURL || process.env.apiUrl || '';
+ axios.defaults.baseURL = baseUrl;
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 let config = {
-   baseURL: process.env.baseURL || process.env.apiUrl || "http://localhost:8888",
+   //baseURL: process.env.baseURL || process.env.apiUrl || "http://localhost:8888",
    timeout: 60 * 1000, // Timeout
   // withCredentials: true, // Check cross-site Access-Control
    headers: {
